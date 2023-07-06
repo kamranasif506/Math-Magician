ea290b851b98a05bc64d13bd23aa31510cf21f78
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-07-06 13:24:22 +0500

    fetch quotes from Ninja API and show it after calculator

---
+++ src/quote/Quote.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react';
+
+function Quotes() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=funny', {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': 'pc77qi3GFj715AjvLqiVQw==PhSGJl3FOGua07tR',
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="container mt-5 bg-light p-2">
+        <div className="row">
+          <div className="col-12">
+            <p className="text-center">Loading...</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+
+      <div className="container mt-5 bg-light p-2">
+        <div className="row">
+          <div className="col-12">
+            <p className="text-center">
+              Error:
+              {error}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+
+    <div className="container mt-5 bg-light p-2">
+      <div className="row">
+        <div className="col-12">
+          <h3 className="text-center">
+            Quotes of the Day
+          </h3>
+          {data.map((item) => (
+            <p key={item.author} className="text-center mt-2">
+              {item.quote}
+              {' '}
+              by
+              {' '}
+              <b>{`"${item.author}"`}</b>
+            </p>
+
+          ))}
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Quotes;
